d38eb0e14ddfacc5ca2152fcbc9e0c7f6cf16f51
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2022-10-20 09:11:43 +0200

    Remove goerli
    Co-authored-by: Alex Peterson <alex@visnovaventures.com>

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -40,7 +40,7 @@ exec besu --rpc-ws-host='0.0.0.0' \
   --data-storage-format=$STORAGE_FORMAT \
   --metrics-enabled \
   --metrics-host='0.0.0.0' \
-  --data-path=/var/lib/goerli-besu \
+  --data-path=/var/lib/besu \
   --sync-mode=$SYNC_MODE \
   --rpc-http-max-active-connections=$MAX_HTTP_CONNECTIONS \
   --p2p-port=$P2P_PORT \
